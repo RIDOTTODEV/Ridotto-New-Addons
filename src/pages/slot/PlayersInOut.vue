76942d86ebc7059f8f5092f8551ad2fab9402fe1
vepa <56221612+vepamirzayev@users.noreply.github.com>
2025-09-29 12:35:52 +0300

    Create form

--- src/pages/slot/PlayersInOut.vue
+++ src/pages/slot/PlayersInOut.vue
@@ -99,7 +99,110 @@
           </q-btn>
         </q-bar>
 
-        <q-card-section> </q-card-section>
+        <q-card-section>
+          <q-form @submit="onSubmitForm" ref="form" class="row">
+            <div class="col-6 q-pa-xs">
+              <SearchPlayerInput
+                v-model="formValues.playerId"
+                :placeholder="$t('searchPlayer')"
+                @onSelectPlayer="onSelectFormPlayer"
+                :optionLabel="'value'"
+                :displayedValue="formValues.playerName"
+                @onClear="onClearFormPlayer"
+              />
+            </div>
+            <div class="col-6 q-pa-xs">
+              <q-select
+                v-model="formValues.currencyId"
+                :placeholder="$t('selectCurrency')"
+                :options="currencies"
+                option-value="id"
+                option-label="name"
+                emit-value
+                map-options
+                :rules="[(val) => !!val || $t('requiredField')]"
+                hide-bottom-space
+                class="super-small"
+                behavior="menu"
+                :label="$t('currency')"
+                outlined
+                dense
+              />
+            </div>
+            <div class="col-6 q-pa-xs">
+              <q-select
+                v-model="formValues.transactionCode"
+                :placeholder="$t('selectTransactionCode')"
+                :options="transactionCodes"
+                option-value="value"
+                option-label="label"
+                emit-value
+                map-options
+                :rules="[(val) => !!val || $t('requiredField')]"
+                hide-bottom-space
+                class="super-small"
+                behavior="menu"
+                outlined
+                dense
+              />
+            </div>
+            <div class="col-6 q-pa-xs">
+              <q-select
+                v-model="formValues.transactionType"
+                :placeholder="$t('selectTransactionType')"
+                :options="transactionTypes"
+                option-value="value"
+                option-label="label"
+                emit-value
+                map-options
+                :rules="[(val) => !!val || $t('requiredField')]"
+                hide-bottom-space
+                class="super-small"
+                behavior="menu"
+                outlined
+                dense
+              />
+            </div>
+            <div class="col-6 q-pa-xs">
+              <q-currency-input
+                v-model="formValues.amount"
+                :placeholder="$t('amount')"
+                :rules="[(val) => !!val || $t('requiredField')]"
+              />
+            </div>
+            <div class="col-6 q-pa-xs">
+              <q-input
+                v-model="formValues.floorNo"
+                :placeholder="$t('floorNo')"
+                outlined
+                dense
+                class="super-small"
+              />
+            </div>
+            <div class="col-12 text-right">
+              <q-btn
+                type="button"
+                :label="$t('cancel')"
+                color="negative"
+                icon="cancel"
+                unelevated
+                no-caps
+                no-wrap
+                class="q-mr-sm"
+                flat=""
+              />
+              <q-btn
+                type="submit"
+                :label="$t('submit')"
+                color="green-8"
+                unelevated
+                no-caps
+                no-wrap
+                icon="save"
+              />
+            </div>
+          </q-form>
+        </q-card-section>
       </q-card>
     </q-dialog>
   </q-page>
@@ -107,7 +210,11 @@
 
 <script setup>
 import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
 import { useSlotStore } from 'src/stores/slot-store'
+import { useCurrencyStore } from 'src/stores/currency-store'
+const currencyStore = useCurrencyStore()
+const { currencies } = storeToRefs(currencyStore)
 const slotStore = useSlotStore()
 const filterValues = ref({
   PlayerId: null,
@@ -190,7 +297,6 @@ const createNewTransaction = ref(false)
 
 const formValues = ref({
   playerId: null,
-  /*  playerFullName: null, */
   floorNo: null,
   currencyId: null,
   amount: null,
@@ -218,6 +324,18 @@ const transactionTypes = ref([
     label: 'Withdrawal',
   },
 ])
+
+const onSubmitForm = () => {
+  console.log(formValues.value)
+}
+const onSelectFormPlayer = (val) => {
+  formValues.value.playerId = val.id
+  formValues.value.playerName = val.value
+}
+const onClearFormPlayer = () => {
+  formValues.value.playerId = null
+  formValues.value.playerName = null
+}
 </script>
 
 <style scoped></style>
